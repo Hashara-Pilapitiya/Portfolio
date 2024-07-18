3147cd7d87431c9ef258621467c3d4b4324acecf
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-18 07:30:23 +0530

    Update header component with navigation links

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    
+    <header className="header">
+        <div className='nav'>
+            <Link to='/' className='nav__title'>
+                <h1>Hashara Pilapitiya</h1>
+            </Link>
+            <div className='nav__menu'>
+                <ul>
+                    <li>Intro</li>
+                    <li>About</li>
+                    <li>Experiences</li>
+                    <li>Projects</li>
+                    <li>Courses</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    </header>
   )
 }
 
