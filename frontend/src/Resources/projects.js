cab96c76bc3a66cb538503a5a35654a4c51e55ef
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-18 20:00:32 +0530

    Add projects data and components to display them

---
+++ frontend/src/Resources/projects.js
@@ -0,0 +1,50 @@
+export default projects = [
+    {
+        technologies: ['React-Native', 'Next.JS', 'Node.JS', 'MongoDB'],
+        _id: '1',
+        title: 'TeachNet',
+        description: 'A mobile app for teachers to share resources and lesson plans and students to learn any web development lessons clearly.',
+        image: 'https://img.freepik.com/free-vector/digital-presentation-concept-illustration_114360-8175.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=sph',
+        link: '/'
+    },
+    {
+        technologies: ['React-Native'],
+        _id: '2',
+        title: 'AuraArt',
+        description: 'A mobile app for downloading and sharing any preffed wallpapers for your home screen or any wotk.',
+        image: 'https://img.freepik.com/free-vector/video-upload-concept-illustration_114360-4702.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=ais_user-customized',
+        link: '/'
+    },
+    {
+        technologies: ['React', 'Express.JS', 'Node.JS', 'MongoDB'],
+        _id: '3',
+        title: 'PetParadise',
+        description: 'An E-commerce web app for pet lovers to buy their pets and get to know about other pets.',
+        image: 'https://img.freepik.com/premium-vector/hand-drawn-animal-rescue-illustration_23-2150219130.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177',
+        link: '/'
+    },
+    {
+        technologies: ['React', 'Express.JS', 'Node.JS', 'MongoDB'],
+        _id: '4',
+        title: 'GlobalGist',
+        description: 'A blog web app for sharing any news and articles about any topic. You can also share your own articles, news and blogs, edit and delete them.',
+        image: 'https://img.freepik.com/premium-vector/blog-abstract-concept-vector-illustration_107173-25627.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=sph',
+        link: '/'
+    },
+    {
+        technologies: ['React', 'Express.JS', 'Node.JS', 'MongoDB'],
+        _id: '5',
+        title: 'BookMtLodge',
+        description: 'A web app for booking any real estate and lodges for your vacation or any work. You can also cancel your booking.',
+        image: 'https://img.freepik.com/premium-vector/booking-hotel-concept_118813-7301.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177',
+        link: '/'
+    },
+    {
+        technologies: ['React', 'Express.JS', 'Node.JS', 'MongoDB'],
+        _id: '6',
+        title: 'IntegraAally',
+        description: 'IntegraAllay is a cutting-edge server designed to revolutionize integration testing for front-end developers. Our platform empowers developers to create mock back-end services, simulate API responses, and thoroughly test their front-end applications.',
+        image: 'https://img.freepik.com/free-vector/hand-drawn-flat-design-sql-illustration_23-2149242071.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=ais_user-customized',
+        link: '/'
+    }
+]
