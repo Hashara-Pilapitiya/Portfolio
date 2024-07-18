98f97c6ef065f2ea0f88ba35b4b4f506fea8d739
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-18 20:11:50 +0530

    Add courses data and components to display them

---
+++ frontend/src/Resources/courses.js
@@ -0,0 +1,20 @@
+export const courses = [
+    {
+        _id: '1',
+        title: 'MERN Stack Fundamentals',
+        description: 'A complete guide to learn MERN stack from scratch. This course will help for me to learn the basics of MERN stack and how to build a full-stack web application using MERN stack. It is provided by Great Learning Academy.',
+        image: 'https://img.freepik.com/free-vector/hand-drawn-flat-design-sql-illustration_23-2149245997.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=ais_user-customized'
+    },
+    {
+        _id: '2',
+        title: 'Internship with React',
+        description: 'A complete guide to learn React from scratch. This course will help for me to learn the basics of React and how to build a web application using React. It is provided by CODE Alpha.',
+        image: 'https://img.freepik.com/free-vector/creative-abstract-quantum-illustration_23-2149236239.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177&semt=ais_user-customized'
+    },
+    {
+        _id: '3',
+        title: 'React JS Tutorial',
+        description: 'A complete guide to learn React from scratch. This course will help for me to learn the basics of React and how to build a web application using React. It is provided by Great Learning Academy.',
+        image: 'https://img.freepik.com/premium-vector/quantum-computing-vector-icon-can-be-used-computer-hardware-iconset_717774-52348.jpg?uid=R145816553&ga=GA1.1.406622832.1717139177'
+    }
+]
